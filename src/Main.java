4cfcad82f4db76219b4d867d086f911b1c86ebac
tohage <avgorin89@gmail.com>
2022-05-16 19:13:26 +0300

    finalized3

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Расчет колличества бонусных рублей:");
-        int ontheclientsaccount = 100;
-        int phonerecharge = 1100;
-        int totalamount = ontheclientsaccount + phonerecharge;
+        int onTheClientsAccount = 100;
+        int phoneRecharge = 1100;
+        int totalAmount = onTheClientsAccount + phoneRecharge;
         int x = 1000;
-        System.out.println("На счету у клиента - " + ontheclientsaccount + " p");
-        System.out.println("Пополнение счета на - " + phonerecharge + " p");
-        if (phonerecharge >= 1000) {
-            int bonus = phonerecharge / 100;
-            System.out.println("Итого на счету - " + (totalamount + bonus) + " p");
+        System.out.println("На счету у клиента - " + onTheClientsAccount + " p");
+        System.out.println("Пополнение счета на - " + phoneRecharge + " p");
+        if (phoneRecharge >= 1000) {
+            int bonus = phoneRecharge / 100;
+            System.out.println("Итого на счету - " + (totalAmount + bonus) + " p");
             System.out.println("Начисленные бонусы - " + bonus + " p");
         } else {
-            System.out.println("Итого на счету - " + totalamount + " p");
+            System.out.println("Итого на счету - " + totalAmount + " p");
             System.out.println("Начисленные бонусы - 0 p");
         }
     }
